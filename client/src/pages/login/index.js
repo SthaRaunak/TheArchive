4749a0d490441009115a0552c70d33a702391b3e
Raunak Shrestha <raunaknagendra@gmail.com>
2023-07-18 22:32:20 +0545

    Adding first phase Landing page Design

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -3,25 +3,28 @@ import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
 const SignupSchema = Yup.object().shape({
-  firstName: Yup.string()
+  userName: Yup.string()
     .min(2, 'Too Short!')
     .max(50, 'Too Long!')
     .required('Required'),
-  lastName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
+
   email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string()
+    .min(3, 'Too Short!')
+    .max(30, 'Too Long!')
+    .required('Required'),
 });
 
 export const Login = () => (
   <div>
-    <h1>Signup</h1>
+    
+    <h1>Log In</h1>
     <Formik
       initialValues={{
-        firstName: '',
-        lastName: '',
+        userName: '',
         email: '',
+        password: '',
+
       }}
       validationSchema={SignupSchema}
       onSubmit={values => {
@@ -31,16 +34,17 @@ export const Login = () => (
     >
       {({ errors, touched }) => (
         <Form>
-          <Field name="firstName" />
-          {errors.firstName && touched.firstName ? (
-            <div>{errors.firstName}</div>
+          <Field name="userName" placeholder="Username" />
+          {errors.userName && touched.userName ? (
+            <div>{errors.userName}</div>
           ) : null}
-          <Field name="lastName" />
-          {errors.lastName && touched.lastName ? (
-            <div>{errors.lastName}</div>
-          ) : null}
-          <Field name="email" type="email" />
+          <Field name="email" type="email" placeholder="Email" />
           {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          <Field name="password" placeholder="Password" />
+          {errors.password && touched.password ? (
+            <div>{errors.password}</div>
+          ) : null}
+          
           <button type="submit">Submit</button>
         </Form>
       )}
