411511a04b329c4832a083d6e5548531b04eec6d
Raunak Shrestha <raunaknagendra@gmail.com>
2023-07-26 10:30:51 +0545

    Segregation into routes and controller. Added bcrypt password hashing and prevented registration with an existing phone number

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const Users = require('./models/users')
 require('dotenv').config()
 const connection = require('./db/connection')
 const cors = require('cors')
+const userRoute = require('./routes/users')
 connection()
 
 
@@ -14,27 +15,10 @@ app.use(express.json())
 
 app.use(cors())
  
+app.use("/",userRoute)
 
  
-app.post('/register', async(req, res) => {
- await Users.create(req.body)
- res.json({
- msg: "You are successfully Registered"
- })
-})
 
- app.get('/checkUserExists/:phoneNumber',async(req,res)=>{
-   const data = await Users.findOne({phoneNumber:req.params.phoneNumber});
-   if(data){
-   res.json({
-    msg: "Phone Number already exists"
-   })
- }else[
-    res.json({
-        validPhoneNo: true
-    })
- ]
-})
  
  
 
